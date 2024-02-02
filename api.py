a279f9f121b27643dde1d2c9e143bbdb1e927178
1-Inna-1 <151846574+1-Inna-1@users.noreply.github.com>
2024-02-02 19:45:02 +0700

    Update api.py

--- api.py
+++ api.py
@@ -1,54 +1,76 @@
-import pygame
+import os
+import sys
 import requests
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel
 
-class Map(object):
+SCREEN_SIZE = [600, 450]
+class Map(QWidget):
     def __init__(self):
-        self.x = 92.954818
-        self.y = 55.989698
-        self.zoom = 17
+        super().__init__()
+        self.long = 92.954818
+        self.width = 55.989698
+        self.zoom = 0.001
         self.type = "map"
+        self.get_image()
+        self.initUI()
 
     def ll(self):
-        return str(self.x) + "," + str(self.y)
+        return str(self.long) + "," + str(self.width)
 
-    def update(self, event):
+    def spn(self):
+        return str(self.zoom) + "," + str(self.zoom)
+
+    def get_image(self):
+        map_request = "http://static-maps.yandex.ru/1.x/?ll={ll}&spn={spn}&l={type}".format(ll=self.ll(), spn=self.spn(),
+                                                                                        type=self.type)
+        response = requests.get(map_request)
+        if not response:
+            print("Ошибка выполнения запроса:")
+            print(map_request)
+            print("Http статус:", response.status_code, "(", response.reason, ")")
+            sys.exit(1)
+
+        self.map_file = "map.png"
+        with open(self.map_file, "wb") as file:
+            file.write(response.content)
+
+    def update_pixmap(self):
+        self.pixmap = QPixmap(self.map_file)
+        self.image.setPixmap(self.pixmap)
+
+    def initUI(self):
+        self.setGeometry(100, 100, *SCREEN_SIZE)
+        self.setWindowTitle('Отображение карты')
+        self.image = QLabel(self)
+        self.image.move(0, 0)
+        self.image.resize(600, 450)
+        self.update_pixmap()
+
+    def closeEvent(self, event):
+        os.remove(self.map_file)
+
+    def keyPressEvent(self, event):
         step = 0.001
-        if event.key == pygame.K_PAGEUP and self.zoom < 19:
-            self.zoom += 1
-        elif event.key == pygame.K_PAGEDOWN and self.zoom > 2:
-            self.zoom -= 1
-        elif if event.key() == Qt.Key_A and self.y < 1000:
-            self.x -= step
-        elif event.key == pygame.K_RIGHT and self.x < 1000:
-            self.x += step
-        elif event.key == pygame.K_UP and self.y < 1000:
-            self.y += step
-        elif event.key == pygame.K_DOWN and self.y < 1000:
-            self.y -= step
+        if event.key() == Qt.Key_PageUp and self.zoom < 19:
+            self.zoom += 0.001
+        elif event.key() == Qt.Key_PageDown and self.zoom > 0.001:
+            self.zoom -= 0.001
+        elif event.key() == Qt.Key_Left and self.long < 100:
+            self.long -= step
+        elif event.key() == Qt.Key_Right and self.long < 100:
+            self.long += step
+        elif event.key() == Qt.Key_Up and self.width < 100:
+            self.width += step
+        elif event.key() == Qt.Key_Down and self.width < 100:
+            self.width -= step
+        self.get_image()
+        self.update_pixmap()
 
-def load_map(mp):
-    map_request = "http://static-maps.yandex.ru/1.x/?ll={ll}&z={z}&l={type}".format(ll=mp.ll(), z=mp.zoom, type=mp.type)
-    response = requests.get(map_request)
-    if not response:
-        pass
 
-    map_file = "map.png"
-    with open(map_file, "wb") as file:
-        file.write(response.content)
-    return map_file
-
-pygame.init()
-screen = pygame.display.set_mode((600, 450))
-mp = Map()
-running = True
-while running:
-    event = pygame.event.wait()
-    if event.type == pygame.QUIT:
-        running = False
-    elif event.type == pygame.KEYUP:
-        mp.update(event)
-
-    map_file = load_map(mp)
-    screen.blit(pygame.image.load(map_file), (0, 0))
-    pygame.display.flip()
-pygame.quit()
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    mp = Map()
+    mp.show()
+    sys.exit(app.exec())
